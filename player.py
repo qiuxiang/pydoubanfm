ad4c71dc09c842e5b87316b4a039d078384934b6
Qiu Xiang <xiang.qiu@qq.com>
2014-08-01 01:21:00 +0800

    添加文档注释

--- player.py
+++ player.py
@@ -1,7 +1,10 @@
 # encoding: utf8
 
+"""基于 GStreamer 的音频播放器，兼容 gst0.1 和 gst1.0"""
+
 try:
     from gi.repository import Gst
+
     Gst.init(None)
 
     def make_playbin():
@@ -33,13 +36,17 @@ class Player:
     def message_handler(self, bus, message):
         if message.type == MESSAGE_EOS:
             self.stop()
+
+            # 播放结束钩子
             if hasattr(self, 'on_eos'):
                 self.on_eos()
 
-    def set_volume(self,volume):
+    def set_volume(self, volume):
+        """设置音量，取值 [0, 1]"""
         self.player.set_property('volume', volume)
 
     def set_uri(self, uri):
+        """设置播放源，支持本地文件和 http"""
         self.player.set_property('uri', uri)
 
     def play(self):
@@ -52,4 +59,5 @@ class Player:
         self.player.set_state(STATE_NULL)
 
     def get_state(self):
+        """return: STATE_NULL or STATE_PLAYING or STATE_PAUSED"""
         return self.player.get_state(0)[1]
