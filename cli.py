9a56eb38f4d480045e87e5a75c2f0a433c8f2605
Qiu Xiang <xiang.qiu@qq.com>
2014-12-04 09:51:19 +0800

    update cli.py

--- cli.py
+++ cli.py
@@ -1,10 +1,9 @@
 #!/usr/bin/env python
 import os
 import sys
-import json
 
-from .utils import setting
+from utils import setting
 
 
 os.system('echo \'%s\' | nc localhost %s' % (
-    json.dumps(sys.argv[1:]), setting.get('port')))
+    ' '.join(sys.argv[1:]), setting.get('port')))
