f6b9caf6f7682dab03b2619177e5f1230c8d9a89
Qiu Xiang <xiang.qiu@qq.com>
2014-12-09 10:53:59 +0800

    专辑使用 <>

--- client/protocol.py
+++ client/protocol.py
@@ -84,7 +84,7 @@ class Protocol(TwistedProtocol):
     def on_playlist(self, playlist):
         print('播放列表：')
         for song in playlist:
-            print('  %s - %s（%s）' %(song['artist'], song['title'], song['albumtitle']))
+            print('  %s - %s <%s>' %(song['artist'], song['title'], song['albumtitle']))
         print('')
 
     def on_state(self, state):
