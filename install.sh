969f9d7681c97fffba722644dcb78f04747f9f57
Qiu Xiang <xiang.qiu@qq.com>
2014-12-19 23:55:06 +0800

    create_desktop_entry -> install，添加本地图标生成脚本

--- install.sh
+++ install.sh
@@ -1,51 +1,60 @@
 #!/bin/sh
 
+pwd=`pwd`
+
+source_icon=$pwd/doubanfm/res/icon.png
+for size in 256 128 48 32 24 22 16; do
+  target_dir=~/.local/share/icons/hicolor/${size}x${size}/apps
+  mkdir -p $target_dir
+  convert $source_icon -resize $size $target_dir/doubanfm.png
+done
+
 echo "
 [Desktop Entry]
 Name=豆瓣FM
 Type=Application
 Categories=Audio;Music;Player;AudioVideo;
-Exec=`pwd`/doubanfm/gtk.py
-Icon=`pwd`/doubanfm/res/icon.png
+Exec=$pwd/doubanfm/gtk.py
+Icon=doubanfm
 Actions=SongNotify;Play;Skip;Like;Unlike;Remove;ServerStart;ServerStop;
 
 [Desktop Action SongNotify]
 Name=♬\t当前歌曲
-Exec=`pwd`/doubanfm/cmd.py song_notify
+Exec=$pwd/doubanfm/cmd.py song_notify
 OnlyShowIn=Unity;
 
 [Desktop Action Play]
 Name=〓\t播放 / 暂停
-Exec=`pwd`/doubanfm/cmd.py play
+Exec=$pwd/doubanfm/cmd.py play
 OnlyShowIn=Unity;
 
 [Desktop Action Skip]
 Name=→\t下一首
-Exec=`pwd`/doubanfm/cmd.py skip
+Exec=$pwd/doubanfm/cmd.py skip
 OnlyShowIn=Unity;
 
 [Desktop Action Like]
 Name=♥\t喜欢
-Exec=`pwd`/doubanfm/cmd.py like
+Exec=$pwd/doubanfm/cmd.py like
 OnlyShowIn=Unity;
 
 [Desktop Action Unlike]
 Name=♡\t不再喜欢
-Exec=`pwd`/doubanfm/cmd.py unlike
+Exec=$pwd/doubanfm/cmd.py unlike
 OnlyShowIn=Unity;
 
 [Desktop Action Remove]
 Name=✕\t不再播放
-Exec=`pwd`/doubanfm/cmd.py remove
+Exec=$pwd/doubanfm/cmd.py remove
 OnlyShowIn=Unity;
 
 [Desktop Action ServerStart]
 Name=●\t启动服务
-Exec=`pwd`/doubanfm/srv.py
+Exec=$pwd/doubanfm/srv.py
 OnlyShowIn=Unity;
 
 [Desktop Action ServerStop]
 Name=○\t关闭服务
-Exec=`pwd`/doubanfm/cmd.py exit
+Exec=$pwd/doubanfm/cmd.py exit
 OnlyShowIn=Unity;
 " > ~/.local/share/applications/pydoubanfm.desktop
