544a9ea3a26ed141f562679c4e6fb073f59641d1
Qiu Xiang <xiang.qiu@qq.com>
2014-12-25 18:00:30 +0800

    cli 继承 client/base，实现启动时信息获取

--- doubanfm/cli.py
+++ doubanfm/cli.py
@@ -3,6 +3,17 @@ import os
 import sys
 sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 
-from doubanfm.client.base import Protocol
+from doubanfm.client.base import Protocol as BaseProtocol
 from doubanfm.utils import run_client
+
+
+class Protocol(BaseProtocol):
+    def __init__(self):
+        BaseProtocol.__init__(self)
+
+    def connectionMade(self):
+        BaseProtocol.connectionMade(self)
+        self.transport.write(
+            'user\nstate\nvolume\nkbps\nchannel\nchannels\nsong')
+
 run_client(Protocol())
